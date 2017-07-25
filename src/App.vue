d04e018f30fbdc456bcdd6007547edf22551f0e4
sc0Vu <alk03073135@gmail.com>
2017-07-25 21:19:38 +0800

    Change bulma to v0.4.4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
+<style scope>
+.navbar-start > .navbar-item.active {
+  background-color: #eee
+}
+</style>
+
 <template>
-  <div id="app" class="container">
-    <div class="columns">
+<div id="app" class="container">
+  <nav class="navbar">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" to="/">Vuethwallet</router-link>
+
+      <div class="navbar-burger burger" v-on:click.prevent="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div class="navbar-menu" v-bind:class="{'is-active': isMenuActive}">
+      <div class="navbar-start">
+        <router-link class="navbar-item" to="/wallet-seed">Seed Wallet</router-link>
+        <router-link class="navbar-item" to="/import-keystore">Import Wallet</router-link>
+        <router-link class="navbar-item" to="/contract-transaction">Contract Transaction</router-link>
+        <router-link class="navbar-item" to="/value-transaction">Value Transaction</router-link>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <!-- <div class="columns">
       <div class="column">
         <div>
           <router-link class="button is-primary" to="/">Wallet</router-link>
@@ -10,7 +37,7 @@
           <router-link class="button is-primary" to="/value-transaction">Value Transaction</router-link>
         </div>
       </div>
-    </div>
+    </div> -->
     <div class="columns">
       <div class="column">
         <div class="notification is-info">
@@ -23,10 +50,25 @@
       <router-view></router-view>
     </transition>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      isMenuActive: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      if (this.isMenuActive) {
+        this.isMenuActive = false
+      } else {
+        this.isMenuActive = true
+      }
+    }
+  }
 }
 </script>
