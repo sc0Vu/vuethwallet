e86a527bfdc38bc0bafcf31fbb51e025c4548ad3
sc0Vu <alk03073135@gmail.com>
2017-09-19 16:29:46 +0800

    Add static file server and heroku postinstall

---
+++ bin/static.js
@@ -0,0 +1,21 @@
+var koa = require('koa')
+var static = require('koa-static')
+var path = require('path')
+var app = new koa()
+var reg = /^(?:[\w\/.]+)\.(?:[\w]+)$/
+
+app.use(async function (ctx, next) {
+  if (!reg.test(ctx.path) && ctx.path !== '/') {
+    // due to we use vue router
+    // we send index.html if the requst is not file and not root path
+    // maybe can use kon-send
+    ctx.path = '/'
+  }
+  await next()
+})
+app.use(static(path.join(path.dirname(__dirname), 'dist'), { gzip: true, defer: true }))
+
+var port = process.env.port || 3000
+app.listen(port)
+
+console.log(`Start to serve static files at port: ${port}`)
