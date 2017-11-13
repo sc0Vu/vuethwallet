139df89757321afd177e8e6819db5d9639d6d5aa
sc0Vu <alk03073135@gmail.com>
2017-11-13 15:41:02 +0800

    Add chainId to host.

--- src/config.js
+++ src/config.js
@@ -3,23 +3,28 @@ const config = {}
 config.hosts = {
   main: {
     test: false,
-    rpcUri: 'https://mainnet.infura.io/vuethwallet'
+    rpcUri: 'https://mainnet.infura.io/vuethwallet',
+    chainId: 1
   },
   ropsten: {
     test: true,
-    rpcUri: 'https://ropsten.infura.io/vuethwallet'
+    rpcUri: 'https://ropsten.infura.io/vuethwallet',
+    chainId: 3
   },
   kovan: {
     test: true,
-    rpcUri: 'https://kovan.infura.io/vuethwallet'
+    rpcUri: 'https://kovan.infura.io/vuethwallet',
+    chainId: 42
   },
   rinkeby: {
     test: true,
-    rpcUri: 'https://rinkeby.infura.io/vuethwallet'
+    rpcUri: 'https://rinkeby.infura.io/vuethwallet',
+    chainId: 4
   },
   infura: {
     test: true,
-    rpcUri: 'https://infuranet.infura.io/vuethwallet'
+    rpcUri: 'https://infuranet.infura.io/vuethwallet',
+    chainId: 999
   }
 }
 
